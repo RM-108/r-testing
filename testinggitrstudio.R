2ef657e9eecc79b1ba49d7161d9af7ce0ef2f69c
RM-108 <rasika_mohan@hotmail.com>
2020-08-31 14:02:13 +1000

    trying out the print.date function

--- testinggitrstudio.R
+++ testinggitrstudio.R
@@ -3,3 +3,4 @@ print("this ifle was created within RStudio")
 print("and it now lives within GitHub")
 
 print("trying to make another change to see how version control works with GitHub")
+print.date(x,max=NULL)
